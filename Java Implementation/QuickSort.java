6196a9b72f75478d7c1c82e381115cc3534bb9b6
hasib824 <hasib824@gmail.com>
2017-09-10 19:09:58 +0600

    random

--- Java Implementation/QuickSort.java
+++ Java Implementation/QuickSort.java
@@ -43,7 +43,7 @@ class MyQuickSort
    {
 	   if(start<end)
 	   {
-	     int pIndex = partition(start,end);
+	     int pIndex = RandomizedPartition(start,end);
 		 doSort(start,pIndex-1);
          doSort(pIndex+1,end);		 
 	   }      
